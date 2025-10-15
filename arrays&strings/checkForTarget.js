b495750a4ff3ff8b2dc0aa1076fb8f9faac3a89a
ivgomez <iv.gomez@hotmail.com>
2025-10-14 20:44:07 -0500

    update

--- arrays&strings/checkForTarget.js
+++ arrays&strings/checkForTarget.js
@@ -1,4 +1,4 @@
-function twoSumWithArrayOrdered(nums, target){
+function checkForTarget(nums, target){
   let result = 0, left = 0, right = nums.length - 1
   
   while(left < right){
@@ -15,5 +15,5 @@ function twoSumWithArrayOrdered(nums, target){
   return false
 }
 
-twoSumWithArrayOrdered([1, 2, 4, 6, 8, 9, 14, 15], 13)
-twoSumWithArrayOrdered([2, 4, 5, 8, 10, 14, 16], 11)
+checkForTarget([1, 2, 4, 6, 8, 9, 14, 15], 13)
+checkForTarget([2, 4, 5, 8, 10, 14, 16], 11)
